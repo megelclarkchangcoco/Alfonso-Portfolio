cbc6ef32ddc17dadccaf610f17fb8f0aae703419
skyclarkchangcoco <123862183+megelclarkchangcoco@users.noreply.github.com>
2023-05-28 19:59:33 +0800

    Delete Codes directory

--- Codes/Displayable.java
+++
@@ -1,6 +0,0 @@
-package AlfonsoPortfolioInheritance;
-
-//Interface to represent a displayable object
-interface Displayable {
-	void display();  // Method to display the object
-}
--- Codes/Main.java
+++
@@ -1,8 +0,0 @@
-package AlfonsoPortfolioInheritance;
-
-public class Main {
-    public static void main(String[] args) {
-        PortfolioApp portfolioApp = new PortfolioApp();  // Create an instance of PortfolioApp
-        portfolioApp.run();  // Run the portfolio application
-    }
-}
